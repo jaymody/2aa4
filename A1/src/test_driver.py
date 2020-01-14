0ed1fb242635a42b3e236b5f25839f7d88323935
Jay Mody <modyj@mcmaster.ca>
2020-01-14 15:16:06 -0500

    skeleton for test module

--- A1/src/test_driver.py
+++ A1/src/test_driver.py
@@ -3,3 +3,15 @@
 #  @brief ?
 #  @date ?
 
+# module 1 test
+def date_test():
+    pass
+
+# module 2 test
+def pos_test():
+    pass
+
+# main function call
+if __name__ == "__main__":
+    date_test()
+    pos_test()

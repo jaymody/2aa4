dfd0cf13ed7a301304d2bae7fa076416a86dc402
Jay Mody <modyj@mcmaster.ca>
2020-01-14 14:52:14 -0500

    initial interface setup using datetime module

--- A1/src/date_adt.py
+++ A1/src/date_adt.py
@@ -1,19 +1,72 @@
 ## @file date_adt.py
-#  @author First and Last Name
+#  @author Jay Mody
 #  @brief ?
 #  @date ?
+
+import datetime
+
+# assumptions
+# - no leap years
+# - months follow this number of day schedule [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+# - year may be positive
 
 ## @brief An ADT ...
 class DateT:
 
-  ## @brief ?
-  #  @details ?
-  #  @param m ...
+  ## @brief Constructor for DateT objects.
+  #  @
+  #  @param d
+  #  @param m
+  #  @param y
   def __init__(self, d, m, y):
+    self.__date = datetime.date(y, m, d)
 
-    
   ## @brief ?
   #  @return ?
   def day(self):
+    return self.__date.day
+  
+  ## @brief ?
+  #  @return ?
+  def month(self):
+    return self.__date.month
 
-  # etc.    
+  ## @brief ?
+  #  @return ?
+  def year(self):
+    return self.__date.year
+
+  ## @brief ?
+  #  @return ?
+  def next(self):
+    pass
+
+  ## @brief ?
+  #  @return ?
+  def prev(self):
+    pass
+
+  ## @brief ?
+  #  @return ?
+  def before(self):
+    pass
+
+  ## @brief ?
+  #  @return ?
+  def after(self):
+    pass
+
+  ## @brief ?
+  #  @return ?
+  def equal(self):
+    pass
+
+  ## @brief ?
+  #  @return ?
+  def add_days(self):
+    pass
+
+  ## @brief ?
+  #  @return ?
+  def days_between(self):
+    pass

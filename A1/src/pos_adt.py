0458f9f90f0a1d67d0b47154c0a47ff6c1411749
Jay Mody <modyj@mcmaster.ca>
2020-01-14 15:29:16 -0500

    basic functions and constructor for GPosT module

--- A1/src/pos_adt.py
+++ A1/src/pos_adt.py
@@ -1,4 +1,35 @@
 ## @file pos_adt.py
-#  @author First and Last Name
+#  @author Jay Mody
 #  @brief ?
 #  @date ?
+
+## @brief An ADT ...
+class GPosT:
+
+  ## @brief Constructor for GPosT objects.
+  #  @
+  #  @param lat
+  #  @param long
+  def __init__(self, lat, long):
+    self.__lat = lat
+    self.__long = long
+
+  ## @brief ?
+  #  @return ?
+  def lat(self):
+    return self.__lat
+  
+  ## @brief ?
+  #  @return ?
+  def long(self):
+    return self.__long
+
+  ## @brief ?
+  #  @return ?
+  def west_of(self, p):
+    return self.__long < p.__long
+
+  ## @brief ?
+  #  @return ?
+  def north_of(self, p):
+    return self.__lat > p.__lat

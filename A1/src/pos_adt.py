3ab3d3f31605405daeea1065bf84c88096941160
Jay Mody <modyj@mcmaster.ca>
2020-01-19 00:19:43 -0500

    convert to radians, added distance function

--- A1/src/pos_adt.py
+++ A1/src/pos_adt.py
@@ -3,8 +3,12 @@
 #  @brief ?
 #  @date ?
 
+from math import cos, sin, asin, atan2, radians
+
 ## @brief An ADT ...
 class GPosT:
+    ## Earth's radius in km
+    __R = 6731
 
     ## @brief Constructor for GPosT objects.
     #  @
@@ -17,8 +21,8 @@ class GPosT:
         if not (-90 <= lat and lat <= 90):
             raise ValueError("lat (latitude) must be between -90 and 90")
 
-        self.__lat = lat
-        self.__long = long
+        self.__lat = radians(lat)
+        self.__long = radians(long)
 
     ## @brief ?
     #  @return ?
@@ -31,11 +35,23 @@ class GPosT:
         return self.__long
 
     ## @brief ?
+    #  @param p
     #  @return ?
     def west_of(self, p):
         return self.__long < p.__long
 
     ## @brief ?
+    #  @param p
     #  @return ?
     def north_of(self, p):
         return self.__lat > p.__lat
+
+    ## @brief ?
+    #  @return ?
+    def distance(self, p):
+        delta_long = p.__long - self.__long
+        delta_lat = p.__lat - self.__lat
+        a = sin(0.5 * delta_lat)**2 + cos(self.__lat) * cos(p.__lat) * sin(0.5 * delta_long)
+        c = 2 * atan2(a**0.5, (1-a)**0.5)
+        distance = self.__R * c
+        return distance

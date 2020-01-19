3b2884033c8dc173219f53f7ad4690d5f7cf13ed
Jay Mody <modyj@mcmaster.ca>
2020-01-18 23:36:08 -0500

    2 space tab -> 4 space tab

--- A1/src/date_adt.py
+++ A1/src/date_adt.py
@@ -9,64 +9,64 @@ import datetime
 #  @details An ADT for an idealized naive date, assuming the current Gregorian calendar always was, and always will be, in effect.
 class DateT:
 
-  ## @brief Constructor for DateT objects.
-  #  @param d The day of the month (integer from 1-31)
-  #  @param m The month of the year (integer from 1-12)
-  #  @param y The year
-  def __init__(self, d, m, y):
-    self.__date = datetime.date(y, m, d)
+    ## @brief Constructor for DateT objects.
+    #  @param d The day of the month (integer from 1-31)
+    #  @param m The month of the year (integer from 1-12)
+    #  @param y The year
+    def __init__(self, d, m, y):
+        self.__date = datetime.date(y, m, d)
 
-  ## @brief Gets the day of the month.
-  #  @return The day of the month.
-  def day(self):
-    return self.__date.day
+    ## @brief Gets the day of the month.
+    #  @return The day of the month.
+    def day(self):
+        return self.__date.day
 
-  ## @brief Gets the month of the year.
-  #  @return The month of the year.
-  def month(self):
-    return self.__date.month
+    ## @brief Gets the month of the year.
+    #  @return The month of the year.
+    def month(self):
+        return self.__date.month
 
-  ## @brief Gets the year.
-  #  @return The year.
-  def year(self):
-    return self.__date.year
+    ## @brief Gets the year.
+    #  @return The year.
+    def year(self):
+        return self.__date.year
 
-  ## @brief Returns a date that is 1 day ahead.
-  #  @return A DateT object that is 1 day ahead.
-  def next(self):
-    return self.__date + datetime.timedelta(days=1)
+    ## @brief Returns a date that is 1 day ahead.
+    #  @return A DateT object that is 1 day ahead.
+    def next(self):
+        return self.__date + datetime.timedelta(days=1)
 
-  ## @brief Returns a date that is 1 day behind.
-  #  @return A DateT object that is 1 day behind.
-  def prev(self):
-    return self.__date - datetime.timedelta(days=1)
+    ## @brief Returns a date that is 1 day behind.
+    #  @return A DateT object that is 1 day behind.
+    def prev(self):
+        return self.__date - datetime.timedelta(days=1)
 
-  ## @brief Determines if this date comes before date d.
-  #  @param d A DateT object.
-  #  @return A boolean that is True if this date comes before date d, else False.
-  def before(self, d):
-    return self.__date < d
+    ## @brief Determines if this date comes before date d.
+    #  @param d A DateT object.
+    #  @return A boolean that is True if this date comes before date d, else False.
+    def before(self, d):
+        return self.__date < d
 
-  ## @brief Determines if this date comes after date d.
-  #  @param d A DateT object.
-  #  @return A boolean that is True if this date comes after date d, else False.
-  def after(self, d):
-    return self.__date > d
+    ## @brief Determines if this date comes after date d.
+    #  @param d A DateT object.
+    #  @return A boolean that is True if this date comes after date d, else False.
+    def after(self, d):
+        return self.__date > d
 
-  ## @brief Determines if this date and date d are equal.
-  #  @param d A DateT object.
-  #  @return A boolean that is True if this date and date d are equal, else False.
-  def equal(self, d):
-    return self.__date == d
+    ## @brief Determines if this date and date d are equal.
+    #  @param d A DateT object.
+    #  @return A boolean that is True if this date and date d are equal, else False.
+    def equal(self, d):
+        return self.__date == d
 
-  ## @brief Returns a date that is n days ahead.
-  #  @param n An integer representing the number of days to skip ahead.
-  #  @return A date that is n days ahead.
-  def add_days(self, n):
-    return self.__date + datetime.timedelta(days=n)
+    ## @brief Returns a date that is n days ahead.
+    #  @param n An integer representing the number of days to skip ahead.
+    #  @return A date that is n days ahead.
+    def add_days(self, n):
+        return self.__date + datetime.timedelta(days=n)
 
-  ## @brief Returns the number of days between this day and date d.
-  #  @param d A DateT object.
-  #  @return The number of days between this day and date d (negative if d comes before this date).
-  def days_between(self, d):
-    return (d - self.__date).days
+    ## @brief Returns the number of days between this day and date d.
+    #  @param d A DateT object.
+    #  @return The number of days between this day and date d (negative if d comes before this date).
+    def days_between(self, d):
+        return (d - self.__date).days

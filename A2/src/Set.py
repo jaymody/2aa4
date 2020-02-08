eaab002dbf300d1b684feb00be5cc04fe32573f7
Jay Mody <modyj@mcmaster.ca>
2020-02-08 16:08:18 -0500

    implementation for Set.py without Equality module

--- A2/src/Set.py
+++ A2/src/Set.py
@@ -2,3 +2,25 @@
 #  @author Jay Mody
 #  @brief todo
 #  @date 08/02/20 (dd/mm/yy)
+
+class Set:
+    def __init__(self, sequence):
+        self.__set = set(sequence)
+
+    def add(self, element):
+        self.__set.add(element)
+
+    def rm(self, element):
+        try:
+            self.__set.remove(element)
+        except KeyError:
+            raise ValueError("Element {} is not in set.".format(element))
+
+    def member(self, element):
+        return element in self.__set
+
+    def size(self):
+        return len(self.__set)
+
+    def to_seq(self):
+        return list(self.__set)

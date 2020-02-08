80e3ec6a494ce76cf80c26233d5a3593ed933b14
Jay Mody <modyj@mcmaster.ca>
2020-02-08 16:39:45 -0500

    implementation for Equality module

--- A2/src/Equality.py
+++ A2/src/Equality.py
@@ -2,3 +2,10 @@
 #  @author Jay Mody
 #  @brief todo
 #  @date 08/02/20 (dd/mm/yy)
+
+class Equality:
+    def equals(self, T):
+        raise NotImplementedError
+
+    def __eq__(self, T):
+        return self.equals(T)

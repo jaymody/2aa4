c248c9564f316afea90f8236267573c3f42ab000
Jay Mody <modyj@mcmaster.ca>
2020-02-08 21:34:24 -0500

    implemented MoledculeT

--- A2/src/MoleculeT.py
+++ A2/src/MoleculeT.py
@@ -2,3 +2,27 @@
 #  @author Jay Mody
 #  @brief todo
 #  @date 08/02/20 (dd/mm/yy)
+
+from ChemEntity import ChemEntity
+from Equality import Equality
+from ElmSet import ElmSet
+
+class MoleculeT(ChemEntity, Equality):
+    def __init__(self, n, e):
+        self.__num = n
+        self.__elm = e
+
+    def get_num(self):
+        return self.__num
+
+    def get_elm(self):
+        return self.__elm
+
+    def num_atoms(self, e):
+        return self.__num if e == self.__elm else 0
+
+    def constit_elems(self):
+        return ElmSet(self.__elm)
+
+    def equals(self, m):
+        return self.get_elm() == m.get_elm() and self.get_num() == m.get_num()

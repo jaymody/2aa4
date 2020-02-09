18a06c160b8a1d73fd9b69cb36bb375211fe14e1
Jay Mody <modyj@mcmaster.ca>
2020-02-08 20:39:18 -0500

    implemented ElementT and ChemEntity

--- A2/src/ChemEntity.py
+++ A2/src/ChemEntity.py
@@ -2,3 +2,10 @@
 #  @author Jay Mody
 #  @brief todo
 #  @date 08/02/20 (dd/mm/yy)
+
+class ChemEntity:
+    def num_atoms(self, e):
+        raise NotImplementedError
+
+    def constit_elems(self):
+        raise NotImplementedError

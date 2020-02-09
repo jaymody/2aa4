596243ca53c3c78ae77dd26d3c3565154a29a1b3
Jay Mody <modyj@mcmaster.ca>
2020-02-08 21:53:12 -0500

    implementation for CompountT

--- A2/src/CompoundT.py
+++ A2/src/CompoundT.py
@@ -2,3 +2,23 @@
 #  @author Jay Mody
 #  @brief todo
 #  @date 08/02/20 (dd/mm/yy)
+
+from ChemEntity import ChemEntity
+from Equality import Equality
+from ElmSet import ElmSet
+
+class CompoundT(ChemEntity, Equality):
+    def __init__(self, m):
+        self.__c = m
+
+    def get_molec_set(self):
+        return self.__c
+
+    def num_atoms(self, e):
+        return sum([m.num_atoms() for m in self.get_molec_set().to_seq()])
+
+    def constit_elems(self, e):
+        return ElmSet([m.get_elm() for m in self.get_molec_set().to_seq()])
+
+    def equals(self, D):
+        return self.get_molec_set().equals(D.get_molec_set())

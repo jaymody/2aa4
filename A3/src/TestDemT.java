3b3dfb8aea9f1725df576c42a4067349c00c4af4
Jay Mody <modyj@mcmaster.ca>
2020-03-16 22:45:54 -0400

    better var names

--- A3/src/TestDemT.java
+++ A3/src/TestDemT.java
@@ -14,96 +14,96 @@ import src.PointT;
 import src.DemT;
 
 public class TestDemT {
-    private DemT pos;
-    private DemT neg;
+    private DemT dem1;
+    private DemT dem2;
 
     @Before
     public void setUp() {
-        ArrayList<ArrayList<Integer>> posarr = new ArrayList<ArrayList<Integer>>();
-        posarr.add(new ArrayList<Integer>(Arrays.asList(1,2,3)));
-        posarr.add(new ArrayList<Integer>(Arrays.asList(4,5,6)));
-        pos = new DemT(posarr, 1.0);
+        ArrayList<ArrayList<Integer>> dem1arr = new ArrayList<ArrayList<Integer>>();
+        dem1arr.add(new ArrayList<Integer>(Arrays.asList(1,2,3)));
+        dem1arr.add(new ArrayList<Integer>(Arrays.asList(4,5,6)));
+        dem1 = new DemT(dem1arr, 1.0);
 
-        ArrayList<ArrayList<Integer>> negarr = new ArrayList<ArrayList<Integer>>();
-        negarr.add(new ArrayList<Integer>(Arrays.asList(1,2)));
-        negarr.add(new ArrayList<Integer>(Arrays.asList(-3,-4)));
-        negarr.add(new ArrayList<Integer>(Arrays.asList(5,5)));
-        neg = new DemT(negarr, 3.0);
+        ArrayList<ArrayList<Integer>> dem2arr = new ArrayList<ArrayList<Integer>>();
+        dem2arr.add(new ArrayList<Integer>(Arrays.asList(1,2)));
+        dem2arr.add(new ArrayList<Integer>(Arrays.asList(-3,-4)));
+        dem2arr.add(new ArrayList<Integer>(Arrays.asList(5,5)));
+        dem2 = new DemT(dem2arr, 3.0);
     }
 
     @After
     public void tearDown() {
-        pos = null;
-        neg = null;
+        dem1 = null;
+        dem2 = null;
     }
 
     @Test
     public void testGetScale() {
-        assertEquals(pos.getScale(), 1.0, 0.0);
-        assertEquals(neg.getScale(), 3.0, 0.0);
+        assertEquals(dem1.getScale(), 1.0, 0.0);
+        assertEquals(dem2.getScale(), 3.0, 0.0);
     }
 
     @Test
     public void testGetNumRow() {
-        assertEquals(pos.getNumRow(), 2);
-        assertEquals(neg.getNumRow(), 3);
+        assertEquals(dem1.getNumRow(), 2);
+        assertEquals(dem2.getNumRow(), 3);
     }
 
     @Test
     public void testGetNumCol() {
-        assertEquals(pos.getNumCol(), 3);
-        assertEquals(neg.getNumCol(), 2);
+        assertEquals(dem1.getNumCol(), 3);
+        assertEquals(dem2.getNumCol(), 2);
     }
 
     @Test
     public void testGet() {
-        assertEquals((int)pos.get(new PointT(0, 0)), 1);
-        assertEquals((int)neg.get(new PointT(2, 1)), 5);
+        assertEquals((int)dem1.get(new PointT(0, 0)), 1);
+        assertEquals((int)dem2.get(new PointT(2, 1)), 5);
     }
 
     @Test
     public void testSet() {
-        pos.set(new PointT(0, 0), -100);
-        neg.set(new PointT(2, 1), 0);
+        dem1.set(new PointT(0, 0), -100);
+        dem2.set(new PointT(2, 1), 0);
 
-        assertEquals((int)pos.get(new PointT(0, 0)), -100);
-        assertEquals((int)neg.get(new PointT(2, 1)), 0);
+        assertEquals((int)dem1.get(new PointT(0, 0)), -100);
+        assertEquals((int)dem2.get(new PointT(2, 1)), 0);
     }
 
     @Test
     public void testCount() {
-        assertEquals(pos.count(-1), 0);
-        assertEquals(pos.count(4), 1);
-        assertEquals(neg.count(3), 0);
-        assertEquals(neg.count(5), 2);
+        assertEquals(dem1.count(-1), 0);
+        assertEquals(dem1.count(4), 1);
+        assertEquals(dem2.count(3), 0);
+        assertEquals(dem2.count(5), 2);
     }
 
     @Test
     public void testCountRow() {
-        assertEquals(pos.countRow(-1, 0), 0);
-        assertEquals(pos.countRow(1, 0), 1);
-        assertEquals(neg.countRow(-3, 1), 1);
-        assertEquals(neg.countRow(5, 2), 2);
+        assertEquals(dem1.countRow(-1, 0), 0);
+        assertEquals(dem1.countRow(1, 0), 1);
+        assertEquals(dem2.countRow(-3, 1), 1);
+        assertEquals(dem2.countRow(5, 2), 2);
     }
 
     @Test
     public void testArea() {
-        assertEquals(pos.area(1), 1.0, 0.00000001);
-        assertEquals(pos.area(7), 0.0, 0.00000001);
-        assertEquals(neg.area(5), 6.0, 0.00000001);
-        assertEquals(neg.area(6), 0.0, 0.00000001);
+        assertEquals(dem1.area(1), 1.0, 0.00000001);
+        assertEquals(dem1.area(7), 0.0, 0.00000001);
+        assertEquals(dem2.area(5), 6.0, 0.00000001);
+        assertEquals(dem2.area(6), 0.0, 0.00000001);
     }
 
     @Test
     public void testTotal() {
-        assertEquals(pos.total(), 21);
-        assertEquals(neg.total(), 6);
+        assertEquals(dem1.total(), 21);
+        assertEquals(dem2.total(), 6);
     }
 
     @Test
     public void testMax() {
-        assertEquals(pos.max(), 6);
-        assertEquals(neg.max(), 5);
+        assertEquals(dem1.max(), 6);
+        assertEquals(dem2.max(), 5);
     }
 
     @Test

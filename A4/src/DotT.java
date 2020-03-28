fc128d5a42d01814ac75929fcead253b85e0716a
Jay Mody <modyj@mcmaster.ca>
2020-03-27 20:59:20 -0400

    implemenation for DotT

--- A4/src/DotT.java
+++ A4/src/DotT.java
@@ -4,3 +4,13 @@
  * @brief Enum for the Dot type.
  * @date 25/03/20 (dd/mm/yy)
  */
+
+package src;
+
+/**
+ * @brief Enum for Dot types.
+ * @details R stands for red, G stands for green, B stands for blue, Y stands for yellow.
+ */
+public enum DotT {
+    R, G, B, Y;
+}

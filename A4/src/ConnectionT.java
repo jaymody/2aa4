08681f32ce3c727d31ebf042b153017a331a46b9
Jay Mody <modyj@mcmaster.ca>
2020-04-02 19:28:57 -0400

    connection now holds a set of all the points along the connection'

--- A4/src/ConnectionT.java
+++ A4/src/ConnectionT.java
@@ -9,6 +9,8 @@
 package src;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+
 import src.PointT;
 
 /**
@@ -16,6 +18,7 @@ import src.PointT;
  */
 public class ConnectionT {
     private ArrayList<PointT> p;
+    private HashSet<PointT> s;
 
     /**
      * @brief Constructor for ConnectionT.
@@ -26,24 +29,55 @@ public class ConnectionT {
      */
     public ConnectionT(ArrayList<PointT> points) throws IllegalArgumentException {
         if (points.size() < 2)
-            throw new IllegalArgumentException("Number of points must be 2 or more.");
+            throw new IllegalArgumentException("Number of points must be 2 or more");
+        else if (points.get(0).equals(points.get(1)))
+            throw new IllegalArgumentException("A connection with only 2 points may not be the same point");
 
         for (int i = 1; i < points.size(); i++)
             if (!isValid(points.get(i), points.get(i-1)))
-                throw new IllegalArgumentException("Adjacent points do not form horizontal/vertical lines.");
+                throw new IllegalArgumentException("Adjacent points do not form horizontal/vertical lines");
 
         this.p = points;
+        this.s = new HashSet<PointT>();
+
+        PointT a, b;
+        a = points.get(0);
+        for (int i = 1; i < points.size(); i++) {
+            b = points.get(i);
+
+            int xmin = Math.min(a.row(), b.row());
+            int xmax = Math.max(a.row(), b.row());
+            int ymin = Math.min(a.col(), b.col());
+            int ymax = Math.max(a.col(), b.col());
+
+            for (int x = xmin; x < xmax; x++)
+                for (int y = ymin; y < ymax; y++)
+                    s.add(new PointT(x, y));
+
+            a = b;
+        }
     }
 
     /**
      * @brief Gets the sequence of points.
      * @returns ArrayList of PointT objects.
      */
-    public ArrayList<PointT> points() {
+    public ArrayList<PointT> pointsSeq() {
         ArrayList<PointT> pointscopy = new ArrayList<PointT>();
         for (PointT point : this.p)
             pointscopy.add(new PointT(point.row(), point.col()));
         return pointscopy;
+    }
+
+    /**
+     * @brief Gets the HashSet of points in the connection.
+     * @returns HashSet of PointT objects.
+     */
+    public HashSet<PointT> pointsSet() {
+        HashSet<PointT> HashSetcopy = new HashSet<PointT>();
+        for (PointT point : this.s)
+            HashSetcopy.add(new PointT(point.row(), point.col()));
+        return HashSetcopy;
     }
 
     private boolean isValid(PointT a, PointT b) {
